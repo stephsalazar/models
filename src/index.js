4a0552a7a9790db1c4e72fc93e64019f23e6571b
Lupo Montero <lupomontero@gmail.com>
2019-02-20 18:37:20 -0500

    chore(index): house keeping...

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-// const { Campus, CampusSchema } = require('./models/Campus');
-// const { Cohort, CohortSchema } = require('./models/Cohort');
-// const { Project, ProjectSchema } = require('./models/Project');
-// const { ProjectFeedback, ProjectFeedbackSchema } = require('./models/ProjectFeedback');
-// const { ReviewerSurvey, ReviewerSurveySchema } = require('./models/ReviewerSurvey');
-// const { Topic, TopicSchema } = require('./models/Topic');
-
-
-// module.exports = {
-//   Campus,
-//   CampusSchema,
-//   Cohort,
-//   CohortSchema,
-//   Project,
-//   ProjectSchema,
-//   ProjectFeedback,
-//   ProjectFeedbackSchema,
-//   ReviewerSurvey,
-//   ReviewerSurveySchema,
-//   Topic,
-//   TopicSchema,
-//   validate: (type, value, cb) => {
-//     const model = new module.exports[type](value);
-//     if (typeof cb === 'function') {
-//       return model.validate(cb);
-//     }
-//     // When no args passed to `model.validate()` it returns a promise.
-//     return model.validate();
-//   },
-// };
-
-
 const schemas = require('schemas');
 const createCampusModel = require('./models/Campus');
 const createCohortModel = require('./models/Cohort');
@@ -64,14 +32,15 @@ module.exports = (conn) => {
     TopicSchema,
   };
 
-  models.validate = (type, value, cb) => {
-    const model = new models[type](value);
-    if (typeof cb === 'function') {
-      return model.validate(cb);
-    }
-    // When no args passed to `model.validate()` it returns a promise.
-    return model.validate();
+  return {
+    ...models,
+    validate: (type, value, cb) => {
+      const model = new models[type](value);
+      if (typeof cb === 'function') {
+        return model.validate(cb);
+      }
+      // When no args passed to `model.validate()` it returns a promise.
+      return model.validate();
+    },
   };
-
-  return models;
 };
