5cfd307ca1951a12b765a614101ff06990665c9a
Lupo Montero <lupomontero@gmail.com>
2019-02-20 18:39:19 -0500

    feat(Project): adds Project.findLatest

--- e2e/projects.spec.js
+++ e2e/projects.spec.js
@@ -1,46 +1,81 @@
-// const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
-const Project = require('../src/models/Project');
+const { ProjectSchema } = require('schemas')(mongoose);
+const Project = require('../src/models/Project')(mongoose, ProjectSchema);
 
 
 describe('e2e::projects', () => {
-  // let connection;
-  // let db;
+  const projectJSON = {
+    slug: 'cipher',
+    repo: 'Laboratoria/curricula-js',
+    path: 'projects/01-cipher',
+    version: '1.0.0',
+    parserVersion: '1.1.1',
+    prefix: 1,
+    title: 'Cifrado César',
+    rubric: '2',
+    locale: 'es-ES',
+    track: 'js',
+    skills: {},
+  };
 
   beforeAll(async () => {
-    await mongoose.connect(global.__MONGO_URI__);
-    // connection = await MongoClient.connect(global.__MONGO_URI__);
-    // db = await connection.db(global.__MONGO_DB_NAME__);
+    await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true });
   });
 
   afterAll(async () => {
-    // await connection.close();
-    // await db.close();
     await mongoose.disconnect();
   });
 
-  it.skip('should ...', () => {
-    const project = new Project({
-      slug: 'cipher',
-      repo: 'Laboratoria/curricula-js',
-      path: 'projects/01-cipher',
-      version: '2.2.0',
-      parserVersion: '1.1.1',
-      prefix: 1,
-      title: 'Cifrado César',
-      rubric: '2',
-      locale: 'es-ES',
-      track: 'js',
-      skills: {},
-    });
+  beforeEach(async () => {
+    await mongoose.connection.db.dropDatabase();
+    await Project.createIndexes();
+  });
+
+  it('should allow to save first version of new project', () => {
+    const project = new Project(projectJSON);
+    return project.save();
+  });
+
+  it('should fail to save when project/version already exists', () => {
+    const project = new Project({ ...projectJSON, slug: 'cipher-bar' });
+    const project2 = new Project({ ...projectJSON, slug: 'cipher-bar' });
 
     return project.save()
-      .then((result) => {
-        console.log('result', result);
-        return Project.find().exec();
-      })
+      .then(() => project2.save())
+      .catch(err => expect(err.message).toMatchSnapshot());
+  });
+
+  it('should allow to save new version of existing project', () => {
+    const project = new Project({ ...projectJSON, slug: 'cipher-baz' });
+    const project2 = new Project({ ...projectJSON, slug: 'cipher-baz', version: '2.2.1' });
+
+    return project.save()
+      .then(() => project2.save());
+  });
+
+  it('should allow to save same version of different projects', () => {
+    const project = new Project({ ...projectJSON, slug: 'cipher-qux' });
+    const project2 = new Project({ ...projectJSON, slug: 'cipher-quxx' });
+
+    return project.save()
+      .then(() => project2.save());
+  });
+
+  it('should be able to query projects latest version of each project', () => {
+    const project1 = new Project({ ...projectJSON, slug: 'cipher-1', version: '1.0.0' });
+    const project1Version2 = new Project({ ...projectJSON, slug: 'cipher-1', version: '2.0.0' });
+    const project2 = new Project({ ...projectJSON, slug: 'cipher-2', version: '1.0.0' });
+    const project2Version2 = new Project({ ...projectJSON, slug: 'cipher-2', version: '2.0.0' });
+    const project3 = new Project({ ...projectJSON, slug: 'cipher-3', version: '1.0.0' });
+
+    return project1.save()
+      .then(() => project1Version2.save())
+      .then(() => project2.save())
+      .then(() => project2Version2.save())
+      .then(() => project3.save())
+      .then(() => Project.findLatest())
       .then((docs) => {
-        console.log(docs);
+        expect(docs.map(({ _id, ...doc }) => doc)).toMatchSnapshot();
       });
   });
 });
--- src/models/Project.js
+++ src/models/Project.js
@@ -1,1 +1,27 @@
-module.exports = (conn, ProjectSchema) => conn.model('Project', ProjectSchema);
+module.exports = (conn, ProjectSchema) => {
+  const Project = conn.model('Project', ProjectSchema);
+
+  Project.findLatest = function (slug) {
+    return (slug)
+      ? this.findOne({ slug })
+        .sort({ version: -1 })
+        .limit(1)
+      : this.aggregate([
+        { $sort: { slug: 1, version: -1 } },
+        {
+          $group: {
+            _id: '$slug',
+            id: { $first: '$_id' },
+            slug: { $first: '$slug' },
+            name: { $first: '$title' },
+            latestVersion: { $first: '$version' },
+            versions: { $push: '$version' },
+          },
+        },
+        { $sort: { _id: 1 } },
+      ])
+        .then(docs => docs.map(({ id, ...doc }) => ({ ...doc, _id: id })));
+  };
+
+  return Project;
+};
