59dc96080d814b71cf9483a7adc948030f67f687
Moisés Cachay <xpktro@gmail.com>
2019-11-08 16:16:16 -0500

    refactor(User): adds endorsements virtual

--- src/GraduateProfile.js
+++ src/GraduateProfile.js
@@ -1,4 +1,12 @@
-module.exports = (conn, GraduateProfileSchema) => conn.model(
-  'GraduateProfile',
-  GraduateProfileSchema,
-);
+module.exports = (conn, GraduateProfileSchema) => {
+  GraduateProfileSchema.virtual('endorsements', {
+    ref: 'GraduateProfileEndorsement',
+    localField: 'user',
+    foreignField: 'user',
+  });
+
+  return conn.model(
+    'GraduateProfile',
+    GraduateProfileSchema,
+  );
+};
