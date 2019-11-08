be6daa7b4f0a571b719d5b8ff56ea3dc88c84400
Moisés Cachay <xpktro@gmail.com>
2019-11-08 16:15:58 -0500

    refactor(User): adds graduate profile virtual

--- src/User.js
+++ src/User.js
@@ -2,5 +2,12 @@ const mongoosePaginate = require('mongoose-paginate');
 
 module.exports = (conn, UserSchema) => {
   UserSchema.plugin(mongoosePaginate);
+  UserSchema.virtual('graduateProfile', {
+    ref: 'GraduateProfile',
+    localField: '_id',
+    foreignField: 'user',
+    justOne: true,
+  });
+
   return conn.model('User', UserSchema);
 };
