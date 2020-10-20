7596bc5a4b8a3271878c27df36a9e6e43ed49613
Betsy Vidal <ing.betsyvies@gmail.com>
2020-10-20 12:33:22 -0500

    feat(models): add comments in dropout schema

--- src/schemas/DropoutSchema.js
+++ src/schemas/DropoutSchema.js
@@ -13,19 +13,23 @@ module.exports = (conn) => {
       ref: 'User',
       required: true,
     },
+    // It refers to the stage in which she leave the program, for example: 1st day
     stage: {
       type: String,
       required: true,
     },
+    // It refers to the student code, for example: LIM022222
     studentCode: {
       type: String,
       required: true,
     },
+    // It means to the date of the dropout
     when: {
       type: Date,
       required: true,
       default: Date.now,
     },
+    // It means how she leave the program, Leave the program voluntarily
     type: {
       type: String,
       required: true,
@@ -34,14 +38,17 @@ module.exports = (conn) => {
       type: String,
       required: true,
     },
+    // It means to how the BC team see her
     observations: {
       type: String,
       required: true,
     },
+    // It means if the BC team is sad, because she is leaving
     sad: {
       type: Boolean,
       required: true,
     },
+    // It refers to other reason, for example: coronavirus
     otherReason: {
       type: String,
       required: true,
